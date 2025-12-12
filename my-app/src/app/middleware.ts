db0c4389ccf7fa96e37d51390de73c24f270f7e0
Jfyy-x <fasaiswy502@gmail.com>
2025-12-13 01:49:39 +0700

    fixbug middleware

--- my-app/src/app/middleware.ts
+++ my-app/src/app/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-    const token = req.cookies.get("token")?.value;
+    const token = req.cookies?.get("token")?.value;
     const protectedRoutes = ["/books"];
     const path = req.nextUrl.pathname;
 
