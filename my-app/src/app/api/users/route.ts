d307706e173bf87a0651cda4ea61669f3db3fb70
Jfyy-x <fasaiswy502@gmail.com>
2025-12-16 23:09:07 +0700

    fix

--- my-app/src/app/api/users/route.ts
+++ my-app/src/app/api/users/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
-import { pool } from "@/app/lib/db";
+import { supabase } from "@/app/lib/db";
 
 export async function GET() {
-    const { rows } = await pool.query(`
-        SELECT * FROM users
-    `)
-    return NextResponse.json(rows);
+    const { data } = await supabase
+        .from("users")
+        .select("*");
+
+    return NextResponse.json(data);
 }
